4a4fea5bccd8f3950e04bf6a0c7693d2729a7b7d
Balázs Varga <balazs.varga@wup.hu>
2019-04-17 09:53:54 +0200

    Add 5th riddle

---
+++ RxSwiftRiddles/Riddle5.swift
@@ -0,0 +1,21 @@
+//
+//  Riddle5.swift
+//  RxSwiftRiddles
+//
+//  Created by Balázs Varga on 2019. 04. 17..
+//  Copyright © 2019. W.UP Ltd. All rights reserved.
+//
+
+import Foundation
+import RxSwift
+
+class Riddle5 {
+    /**
+     * Sum up the latest values of [first] and [second] whenever one of the Observables emits a new value.
+     *
+     * Use case: Two input fields in a calculator that need to be summed up and the result should be updated every time one of the inputs change.
+     */
+    func solve(first: Observable<Int>, second: Observable<Int>) -> Observable<Int> {
+        TODO()
+    }
+}
