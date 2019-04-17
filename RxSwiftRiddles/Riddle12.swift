aa4d84b0dffcb04b2782ea42a60afe6bd3e52b6d
Balázs Varga <balazs.varga@wup.hu>
2019-04-17 10:10:33 +0200

    Add 12th riddle

---
+++ RxSwiftRiddles/Riddle12.swift
@@ -0,0 +1,21 @@
+//
+//  Riddle12.swift
+//  RxSwiftRiddles
+//
+//  Created by Balázs Varga on 2019. 04. 17..
+//  Copyright © 2019. W.UP Ltd. All rights reserved.
+//
+
+import Foundation
+import RxSwift
+
+class Riddle12 {
+    /**
+     * In case the [source] Observable emits an error, don't emit the error and instead complete the Observable with a value of 5.
+     *
+     * Use case: Getting a network error and you want to recover and show some default state.
+     */
+    func solve(source: Observable<Int>) -> Observable<Int> {
+        TODO()
+    }
+}
