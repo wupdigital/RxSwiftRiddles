dee939a372f26e42cc2e9c0aa823400646231ada
Balázs Varga <balazs.varga@wup.hu>
2019-04-17 09:49:33 +0200

    Add 3rd riddle

---
+++ RxSwiftRiddles/Riddle3.swift
@@ -0,0 +1,21 @@
+//
+//  Riddle3.swift
+//  RxSwiftRiddles
+//
+//  Created by Balázs Varga on 2019. 04. 17..
+//  Copyright © 2019. W.UP Ltd. All rights reserved.
+//
+
+import Foundation
+import RxSwift
+
+class Riddle3 {
+    /**
+     * Don't emit odd numbers from the [source] Observable.
+     *
+     * Use case: You want to filter certain items out.
+     */
+    func solve(source: Observable<Int>) -> Observable<Int> {
+        TODO()
+    }
+}
