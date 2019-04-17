5ae359fd4a3406076d80e1411b3b8d55019e3aa9
Balázs Varga <balazs.varga@wup.hu>
2019-04-17 09:58:44 +0200

    Add  8th riddle

---
+++ RxSwiftRiddles/Riddle8.swift
@@ -0,0 +1,21 @@
+//
+//  Riddle8.swift
+//  RxSwiftRiddles
+//
+//  Created by Balázs Varga on 2019. 04. 17..
+//  Copyright © 2019. W.UP Ltd. All rights reserved.
+//
+
+import Foundation
+import RxSwift
+
+class Riddle8 {
+    /**
+     * Delay the entire [source] by 200ms. This includes subscribing, emissions and terminal events.
+     *
+     * Use case: Make an Observable "lazy" for some time. For instance, when wanting to postpone some UI action.
+     */
+    func solve(source: Observable<Void>) -> Observable<Void> {
+        TODO()
+    }
+}
