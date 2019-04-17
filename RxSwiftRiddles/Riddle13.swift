200c066e019326dc319a656ab6ec8845cee26647
Balázs Varga <balazs.varga@wup.hu>
2019-04-17 10:12:41 +0200

    Add 13th riddle

---
+++ RxSwiftRiddles/Riddle13.swift
@@ -0,0 +1,21 @@
+//
+//  Riddle13.swift
+//  RxSwiftRiddles
+//
+//  Created by Balázs Varga on 2019. 04. 17..
+//  Copyright © 2019. W.UP Ltd. All rights reserved.
+//
+
+import Foundation
+import RxSwift
+
+class Riddle13 {
+    /**
+     * When the [source] emits the same value as it did last time, don't allow it to travel downstream.
+     *
+     * Use case: You only want to observe changes of a value but don't care if the same value has been emitted consecutively.
+     */
+    func solve(source: Observable<Int>) -> Observable<Int> {
+        TODO()
+    }
+}
