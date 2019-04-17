9dfba91621de27e781cbc13e3a571657c2d7bab0
Balázs Varga <balazs.varga@wup.hu>
2019-04-17 08:51:44 +0200

    Xcode project init

---
+++ RxSwiftRiddles/RxSwiftRiddles.h
@@ -0,0 +1,19 @@
+//
+//  RxSwiftRiddles.h
+//  RxSwiftRiddles
+//
+//  Created by Balázs Varga on 2019. 04. 17..
+//  Copyright © 2019. W.UP Ltd. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for RxSwiftRiddles.
+FOUNDATION_EXPORT double RxSwiftRiddlesVersionNumber;
+
+//! Project version string for RxSwiftRiddles.
+FOUNDATION_EXPORT const unsigned char RxSwiftRiddlesVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RxSwiftRiddles/PublicHeader.h>
+
+
