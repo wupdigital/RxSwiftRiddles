32c1c0e42e493a6bc9e30e2a667d8216422c9ce1
Balázs Varga <balazs.varga@wup.hu>
2019-04-17 09:52:30 +0200

    Add 4th riddle

---
+++ RxSwiftRiddles/Riddle4.swift
@@ -0,0 +1,23 @@
+//
+//  Riddle4.swift
+//  RxSwiftRiddles
+//
+//  Created by Balázs Varga on 2019. 04. 17..
+//  Copyright © 2019. W.UP Ltd. All rights reserved.
+//
+
+import Foundation
+import RxSwift
+
+class Riddle4 {
+
+    /**
+     * Implement a toggle mechanism. Initially we want to return false.
+     * Every time [source] emits, we want to negate the previous value.
+     *
+     * Use case: Some button that can toggle two states. For instance a switch between White & Dark theme.
+     */
+    func solve(source: Observable<Void>) -> Observable<Bool> {
+        TODO()
+    }
+}
