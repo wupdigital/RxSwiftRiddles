5cfc2cbe419e79cc6fcdf5a72956fe6d774a480f
Balázs Varga <balazs.varga@wup.hu>
2019-04-17 09:45:57 +0200

    Add first riddle

---
+++ RxSwiftRiddles/Riddle1.swift
@@ -0,0 +1,22 @@
+//
+//  Riddle1.swift
+//  RxSwiftRiddles
+//
+//  Created by Balázs Varga on 2019. 04. 17..
+//  Copyright © 2019. W.UP Ltd. All rights reserved.
+//
+
+import Foundation
+import RxSwift
+
+class Riddle1 {
+    
+    /**
+     * Transform the given [value] into an Observable that emits the value and then completes.
+     *
+     * Use case: You want to transform some value to the reactive world.
+     */
+    func solve(value: Int) -> Observable<Int> {
+        TODO()
+    }
+}
---
+++ RxSwiftRiddles/TODO.swift
@@ -0,0 +1,13 @@
+//
+//  TODO.swift
+//  RxSwiftRiddles
+//
+//  Created by Balázs Varga on 2019. 04. 17..
+//  Copyright © 2019. W.UP Ltd. All rights reserved.
+//
+
+import Foundation
+
+func TODO() -> Never {
+    fatalError("Not implemented")
+}
