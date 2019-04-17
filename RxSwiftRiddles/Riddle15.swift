54da1199d975ca6d57927f28036fe90a22ec0512
Balázs Varga <balazs.varga@wup.hu>
2019-04-17 10:18:00 +0200

    Add 15th riddle

---
+++ RxSwiftRiddles/Riddle15.swift
@@ -0,0 +1,21 @@
+//
+//  Riddle15.swift
+//  RxSwiftRiddles
+//
+//  Created by Balázs Varga on 2019. 04. 17..
+//  Copyright © 2019. W.UP Ltd. All rights reserved.
+//
+
+import Foundation
+import RxSwift
+
+class Riddle15 {
+    /**
+     * Concatenate the [first] Observable with the [second] while subscribing to both early.
+     *
+     * Use case: You have two sources of your data (cache & network request). You want to subscribe to both right away and keep the emission order.
+     */
+    func solve(first: Observable<Int>, second: Observable<Int>) -> Observable<Int> {
+        TODO()
+    }
+}
