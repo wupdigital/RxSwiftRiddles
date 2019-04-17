d28916556cf5d22e9599c827995dec24e5e0f589
Balázs Varga <balazs.varga@wup.hu>
2019-04-17 09:48:16 +0200

    Add second riddle

---
+++ RxSwiftRiddles/Riddle2.swift
@@ -0,0 +1,22 @@
+//
+//  Riddle2.swift
+//  RxSwiftRiddles
+//
+//  Created by Balázs Varga on 2019. 04. 17..
+//  Copyright © 2019. W.UP Ltd. All rights reserved.
+//
+
+import Foundation
+import RxSwift
+
+class Riddle2 {
+    
+    /**
+     * Increment each emitted value of the given [source] by 1.
+     *
+     * Use case: You want to transform the data.
+     */
+    func solve(source: Observable<Int>) -> Observable<Int> {
+        TODO()
+    }
+}
