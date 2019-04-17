c693b9678e46ed3e884edbb255c71abb7d762137
Balázs Varga <balazs.varga@wup.hu>
2019-04-17 09:57:22 +0200

    Add 7th riddle

---
+++ RxSwiftRiddles/Riddle7.swift
@@ -0,0 +1,21 @@
+//
+//  Riddle7.swift
+//  RxSwiftRiddles
+//
+//  Created by Balázs Varga on 2019. 04. 17..
+//  Copyright © 2019. W.UP Ltd. All rights reserved.
+//
+
+import Foundation
+import RxSwift
+
+class Riddle7 {
+    /**
+     * When the [source] emits the same value multiple times, only allow the first value to travel downstream.
+     *
+     * Use case: You never want to show the same value twice.
+     */
+    func solve(source: Observable<Int>) -> Observable<Int> {
+        TODO()
+    }
+}
